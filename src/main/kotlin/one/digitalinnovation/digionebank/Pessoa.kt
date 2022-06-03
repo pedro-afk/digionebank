3a060a9844a7b3d96fab145df85c796846cabe18
pedro-afk <pedrodaniel0020@gmail.com>
2022-06-03 07:53:28 -0300

    updated to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,8 +4,12 @@ class Pessoa {
     var nome:String = "Pedro"
     var cpf: String = "123.123.123.12"
 
-    // utilizando o private nos setters, evita que os atributos sejam alterados na execução da aplicação
+    // utilizando o private nos setters, evita que o valor dos atributos sejam alterados na execução da aplicação
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 
     // opção para criar classe dentro de outra classe, ATENÇÃO (Isso é feito em casos específicos)
     // em outros casos, criaremos outra classe, depende muito da necessidade
@@ -24,5 +28,6 @@ fun main() {
 
 
     println("${pedro.nome} ${pedro.cpf} ${pedro.Endereco().rua}")
+    println(pedro.pessoaInfo())
 
 }
